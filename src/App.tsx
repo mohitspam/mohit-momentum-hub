cc69010a3a0962d606b99009a647ed90ace15a98
mohitspam <3gkotta@gmail.com>
2025-06-14 14:27:10 +0530

    Update App.tsx - app is already in pages, so the directory of login should be ./login and not ./src/login

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
+import Login from "./Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const queryClient = new QueryClient();
